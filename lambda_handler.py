9469e639e0c73d4f5cabbb6c8f6d0115ddd87796
Kha Pham <Khathepham@gmail.com>
2023-07-08 00:22:03 -0400

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,4 +1,4 @@
-import json;
+import json
 import os
 
 import main
@@ -6,19 +6,20 @@ import main
 
 def lambda_handler(event, context):
     body = ""
+    pathHeader = "/battlesneks_dev"
     try:
-        if event['path'] == "/" and event['httpMethod'] == "GET":
+        if event['path'] == f"{pathHeader}/" and event['httpMethod'] == "GET":
             body = json.dumps(main.info())
 
         elif event['httpMethod'] == 'POST':
-            if event['path'] == "/start":
+            if event['path'] == f"{pathHeader}/start":
                 game_state = event['body']
                 main.start(json.loads(game_state))
                 body = "ok"
-            elif event['path'] == "/move":
+            elif event['path'] == f"{pathHeader}/move":
                 game_state = event['body']
                 body = json.dumps(main.move(json.loads(game_state)))
-            elif event['path'] == "/end":
+            elif event['path'] == f"{pathHeader}/end":
                 game_state = event['body']
                 main.end(json.loads(game_state))
                 body = "ok"
@@ -40,8 +41,7 @@ def lambda_handler(event, context):
                 "Content-Type": "application/json"
             },
             'body': json.dumps({
-                "Region ": json_region
+                "Path": event['path'],
+                "HTTPMethod": event['httpMethod']
             })
         }
-
-
