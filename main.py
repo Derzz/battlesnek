7735c27a3fc276979ddcbf63420af292c7d159cb
Will01liMEH <137914508+Will01liMEH@users.noreply.github.com>
2023-07-17 23:08:33 -0400

    Changes to README.md and slight change to main.py
    Added multi run commands to README.md, cleared out clutter in main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@
 import random
 import typing
 
-
 # info is called when you create your Battlesnake on play.battlesnake.com
 # and controls your Battlesnake's appearance
 # TIP: If you open your Battlesnake URL in a browser you should see this data
@@ -59,8 +58,6 @@ def move(game_state: typing.Dict) -> typing.Dict:
     board_width = game_state['board']['width']
     board_height = game_state['board']['height']
 
-    fed = 0
-
     x = my_head["x"]
     y = my_head["y"]
     px = my_neck["x"]
@@ -75,38 +72,35 @@ def move(game_state: typing.Dict) -> typing.Dict:
     dx = my_head["x"]
     dy = my_head["y"] - 1
 
-    move_list_1 = calc_move(game_state, x, y, px, py, board_width, board_height, fed)
-    if (is_single_move(move_list_1)):
+    move_list_1 = calc_move (game_state, x, y, px, py, board_width, board_height)
+    if (is_single_move (move_list_1)):
         next_move = move_list_1
-    elif (len(move_list_1) > 0):
-        next_move = random.choice(move_list_1)
+    elif (len (move_list_1) > 0):
+        next_move = random.choice (move_list_1)
     else:
-        next_move = end_it(game_state)
+        next_move = end_it (game_state)
 
-    # Food hunting
+    #Food hunting
     food = game_state['board']['food']
     for pellet in food:
         if is_move_safe['right'] and [rx, ry] == [pellet['x'], pellet['y']]:
             next_move = "right"
-            fed = 1
             break
         elif is_move_safe['left'] and [lx, ly] == [pellet['x'], pellet['y']]:
             next_move = "left"
-            fed = 1
             break
         elif is_move_safe['up'] and [ux, uy] == [pellet['x'], pellet['y']]:
             next_move = "up"
-            fed = 1
             break
         elif is_move_safe['down'] and [dx, dy] == [pellet['x'], pellet['y']]:
             next_move = "down"
-            fed = 1
             break
         else:
-            fed = 0
-
+            if game_state["you"]["health"] == 1:
+                return {"move": end_it}
+    
     # Tail chasing
-    if game_state["you"]["health"] > 25 and game_state["you"]["length"] >= 5:
+    if game_state["you"]["health"] > 25 and game_state["you"]["health"] != 100 and game_state["you"]["length"] % 2 == 0:
         if is_move_safe['right'] and [rx, ry] == [my_tail['x'], my_tail['y']]:
             next_move = "right"
         if is_move_safe['left'] and [lx, ly] == [my_tail['x'], my_tail['y']]:
@@ -116,53 +110,9 @@ def move(game_state: typing.Dict) -> typing.Dict:
         if is_move_safe['down'] and [dx, dy] == [my_tail['x'], my_tail['y']]:
             next_move = "down"
 
-    # move_list_2 = []
-    # if is_single_move (move_list_1):
-    #     print ("Case 1")
-    #     next_move = move_list_1
-    #     if move_list_1 == "right" and not foresight(game_state, rx, ry, x, y, board_width, board_height):
-    #         # He's screwed.
-    #             print(f"MOVE {game_state['turn']}: Oh no. ")
-    #     elif move_list_1 == "left" and not foresight(game_state, lx, ly, x, y, board_width, board_height):
-    #         # He's screwed.
-    #             print(f"MOVE {game_state['turn']}: Oh no. ")
-    #     elif move_list_1 == "up" and not foresight(game_state, ux, uy, x, y, board_width, board_height):
-    #         # He's screwed.
-    #             print(f"MOVE {game_state['turn']}: Oh no. ")
-    #     elif move_list_1 == "down" and not foresight(game_state, dx, dy, x, y, board_width, board_height):
-    #         # He's screwed.
-    #             print(f"MOVE {game_state['turn']}: Oh no. ")
-    # elif len (move_list_1) > 0:
-    #     print ("Case 2")
-    #     for move in move_list_1:
-    #         if move == "right" and not foresight(game_state, rx, ry, x, y, board_width, board_height):
-    #             print ("2.1")
-    #             is_move_safe ["right"] = False
-    #         elif move == "left" and not foresight(game_state, lx, ly, x, y, board_width, board_height):
-    #             print ("2.2")
-    #             is_move_safe ["left"] = False
-    #         elif move == "up" and not foresight(game_state, ux, uy, x, y, board_width, board_height):
-    #             print ("2.3")
-    #             is_move_safe ["up"] = False
-    #         elif move == "down" and not foresight(game_state, dx, dy, x, y, board_width, board_height):
-    #             print ("2.4")
-    #             is_move_safe ["down"] = False
-    #     for move, isSafe in is_move_safe.items():
-    #         if isSafe:
-    #             move_list_2.append(move)
-    #     if is_single_move (move_list_2):
-    #         next_move = move_list_2
-    #     elif (len(move_list_2) > 0):
-    #         next_move = random.choice (move_list_2)
-    #     else:
-    #         # He's screwed.
-    #         print(f"MOVE {game_state['turn']}: Oh no. ")
-    #         next_move = random.choice (move_list_1)
-    # else:
-    #     print ("Case 3")
-    #     print(f"MOVE {game_state['turn']}: I will go out on my own terms!")
-    #     next_move = end_it (game_state)
-
+    if game_state["you"]["health"] == 1:
+        next_move = end_it
+    
     # TODO: Step 3A - Don't ram into heads where opponent.snake.len > ours
 
     # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
@@ -171,13 +121,12 @@ def move(game_state: typing.Dict) -> typing.Dict:
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
 
-
-def check_move(game_state: typing.Dict, x, y, px, py, board_width, board_height, fed):
+def check_move (game_state: typing.Dict, x, y, px, py, board_width, board_height):
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
     my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
 
     is_move_safe = {"up": True, "down": True, "left": True, "right": True}
-
+    
     if px < x:  # Neck is left of head, don't move left
         is_move_safe["left"] = False
     elif px > x:  # Neck is right of head, don't move right
@@ -186,34 +135,33 @@ def check_move(game_state: typing.Dict, x, y, px, py, board_width, board_height,
         is_move_safe["down"] = False
     elif py > y:  # Neck is above head, don't move up
         is_move_safe["up"] = False
-
+        
     battlesnakes = game_state['board']['snakes']
     for snake in battlesnakes:
-        for bodyCoord in snake['body'][:(fed - 1)]:
-            if is_move_safe['right'] and ([x + 1, y] == [bodyCoord['x'], bodyCoord['y']] or x + 1 >= board_width - 1):
+        for bodyCoord in snake['body'][:-1]:
+            if is_move_safe['right'] and ([x+1, y] == [bodyCoord['x'], bodyCoord['y']] or x+1 >= board_width - 1):
                 is_move_safe['right'] = False
-            if is_move_safe['left'] and ([x - 1, y] == [bodyCoord['x'], bodyCoord['y']] or x - 1 <= 0):
+            if is_move_safe['left'] and ([x-1, y] == [bodyCoord['x'], bodyCoord['y']] or x-1 <= 0):
                 is_move_safe['left'] = False
-            if is_move_safe['up'] and ([x, y + 1] == [bodyCoord['x'], bodyCoord['y']] or y + 1 >= board_height - 1):
+            if is_move_safe['up'] and ([x, y+1] == [bodyCoord['x'], bodyCoord['y']] or y+1 >= board_height - 1):
                 is_move_safe['up'] = False
-            if is_move_safe['down'] and ([x, y - 1] == [bodyCoord['x'], bodyCoord['y']] or y - 1 <= 0):
+            if is_move_safe['down'] and ([x, y-1] == [bodyCoord['x'], bodyCoord['y']] or y-1 <= 0):
                 is_move_safe['down'] = False
-
+                
     safe_moves = []
     for move, isSafe in is_move_safe.items():
         if isSafe:
             safe_moves.append(move)
     return safe_moves
 
-
-def check_move_failsafe(game_state: typing.Dict, x, y, px, py, board_width, board_height, fed):
-    print("Failsafe on")
-
+def check_move_failsafe (game_state: typing.Dict, x, y, px, py, board_width, board_height):
+    print ("Failsafe on")
+    
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
     my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
 
     is_move_safe = {"up": True, "down": True, "left": True, "right": True}
-
+    
     if px < x:  # Neck is left of head, don't move left
         is_move_safe["left"] = False
     elif px > x:  # Neck is right of head, don't move right
@@ -222,17 +170,17 @@ def check_move_failsafe(game_state: typing.Dict, x, y, px, py, board_width, boar
         is_move_safe["down"] = False
     elif py > y:  # Neck is above head, don't move up
         is_move_safe["up"] = False
-
+        
     battlesnakes = game_state['board']['snakes']
     for snake in battlesnakes:
-        for bodyCoord in snake['body'][:(fed - 1)]:
-            if [x + 1, y] == [bodyCoord['x'], bodyCoord['y']] or x + 1 > board_width - 1:
+        for bodyCoord in snake['body'][:-1]:
+            if [x+1, y] == [bodyCoord['x'], bodyCoord['y']] or x+1 > board_width - 1:
                 is_move_safe['right'] = False
-            if [x - 1, y] == [bodyCoord['x'], bodyCoord['y']] or x - 1 < 0:
+            if [x-1, y] == [bodyCoord['x'], bodyCoord['y']] or x-1 < 0:
                 is_move_safe['left'] = False
-            if [x, y + 1] == [bodyCoord['x'], bodyCoord['y']] or y + 1 > board_height - 1:
+            if [x, y+1] == [bodyCoord['x'], bodyCoord['y']] or y+1 > board_height - 1:
                 is_move_safe['up'] = False
-            if [x, y - 1] == [bodyCoord['x'], bodyCoord['y']] or y - 1 < 0:
+            if [x, y-1] == [bodyCoord['x'], bodyCoord['y']] or y-1 < 0:
                 is_move_safe['down'] = False
     safe_moves = []
     for move, isSafe in is_move_safe.items():
@@ -240,35 +188,31 @@ def check_move_failsafe(game_state: typing.Dict, x, y, px, py, board_width, boar
             safe_moves.append(move)
     return safe_moves
 
+def calc_move (game_state: typing.Dict, x, y, px, py, board_width, board_height):
+    safe_moves = check_move (game_state, x, y, px, py, board_width, board_height)
 
-def calc_move(game_state: typing.Dict, x, y, px, py, board_width, board_height, fed):
-    safe_moves = check_move(game_state, x, y, px, py, board_width, board_height, fed)
-
-    if len(safe_moves) > 0:
+    if len (safe_moves) > 0:
         return safe_moves
     else:
-        safe_moves = check_move_failsafe(game_state, x, y, px, py, board_width, board_height, fed)
-        if (len(safe_moves) > 0):
+        safe_moves = check_move_failsafe (game_state, x, y, px, py, board_width, board_height)
+        if (len (safe_moves) > 0):
             return safe_moves
         else:
-            return end_it(game_state)
+            return end_it (game_state)
 
-
-def foresight(game_state: typing.Dict, x, y, px, py, board_width, board_height, fed):
+def foresight (game_state: typing.Dict, x, y, px, py, board_width, board_height):
     battlesnakes = game_state['board']['snakes']
     for snake in battlesnakes:
-        for bodyCoord in snake['body'][:(fed - 1)]:
-            return len(calc_move(game_state, x, y, px, py, board_width, board_height, fed)) > 0
+        for bodyCoord in snake['body'][:-1]:
+            return len (calc_move (game_state, x, y, px, py, board_width, board_height)) > 0
 
-
-def is_single_move(var):
-    if var == "right" or var == "left" or var == "up" or var == "down":
+def is_single_move (var):
+    if var == "right" or  var == "left" or  var == "up" or  var == "down":
         return True
     else:
         return False
 
-
-def end_it(game_state: typing.Dict):
+def end_it (game_state: typing.Dict):
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
     my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
     # End it
@@ -284,9 +228,8 @@ def end_it(game_state: typing.Dict):
     else:
         return "down"
 
-
 # Start server when `python main.py` is run
-if __name__ == "__main__":
+if __name__ == "__main__":  
     from server import run_server
 
-    run_server({"info": info, "start": start, "move": move, "end": end})
+    run_server({"info": info, "start": start, "move": move, "end": end}, 8000)
