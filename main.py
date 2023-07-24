493d7e0d16f3b81b3412602f160d8133abd19486
Kha Pham <Khathepham@gmail.com>
2023-07-24 14:03:46 -0400

    a* implimentation

--- main.py
+++ main.py
@@ -37,97 +37,97 @@ def start (game_state):
     """
     print ("GAME START")
 
-
+#
 # move is called on every turn and returns your next move
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
-# def move(game_state: typing.Dict) -> typing.Dict:
-#     # Operate A* here
-#     if game_state['you']['length'] < 5:
-#         return priorASearch.food(game_state)
-#     # Else do flood fill strat
-#     else:
-#         print("snake at 5 or more, doing strat")
-#         return {"move": "down"}
+def move(game_state: typing.Dict) -> typing.Dict:
+    # Operate A* here
+    if game_state['you']['length'] < 5:
+        return priorASearch.food(game_state)
+    # Else do flood fill strat
+    else:
+        print("snake at 5 or more, doing strat")
+        return {"move": "down"}
 
 
-def move (game_state):
-    """
-    Called when the Battlesnake Engine needs to know your next move.
-    The data parameter will contain information about the board.
-    Your response must include your move of up, down, left, or right.
-    """
-    game_state
-    move = ""
-    up_area = floodFill (getNextPosition ("up", game_state), game_state, arrayify (game_state, not Beeg (game_state)))
-    down_area = floodFill (getNextPosition ("down", game_state), game_state, arrayify (game_state, not Beeg (game_state)))
-    right_area = floodFill (getNextPosition ("right", game_state), game_state, arrayify (game_state, not Beeg (game_state)))
-    left_area = floodFill (getNextPosition ("left", game_state),  game_state, arrayify (game_state, not Beeg (game_state)))
-    move_area = [up_area, down_area, right_area, left_area]
-
-    if len (game_state["board"]["food"]) > 0:
-        print ("Current position: {}".format (game_state["you"]["head"]))
-        print ("Closest Food: {}".format (findFood (game_state)))
-        move = goto (move_area, findFood (game_state), game_state)
-
-    if is_stuck (move_area, game_state) and max (move_area) != 0:
-        if up_area == max (move_area):
-            move = "up"
-        elif down_area == max (move_area):
-            move = "down"
-        elif left_area == max (move_area):
-            move = "left"
-        elif right_area == max (move_area):
-            move = "right"
-
-    # Failsafe on
-    if is_stuck (move_area, game_state) and max (move_area) == 0:
-        print ("Failsafe triggered")
-
-    # Ignores projected heads
-    if max (move_area) == 0:
-        print ("Head prediction off")
-        up_area = floodFill (getNextPosition ("up", game_state), game_state, arrayify (game_state, False))
-        down_area = floodFill (getNextPosition ("down", game_state), game_state, arrayify (game_state, False))
-        right_area = floodFill (getNextPosition ("right", game_state), game_state, arrayify (game_state, False))
-        left_area = floodFill (getNextPosition ("left", game_state),  game_state, arrayify (game_state, False))
-        move_area = [up_area, down_area, right_area, left_area]
-
-    if move == "":
-        goodMoves = []
-
-        if up_area == max (move_area):
-            goodMoves.append ("up")
-
-        if down_area == max (move_area):
-            goodMoves.append ("down")
-
-        if left_area == max (move_area):
-            goodMoves.append ("left")
-
-        if right_area == max (move_area):
-            goodMoves.append ("right")
-
-        if (len (goodMoves) > 0):
-            move = random.choice (goodMoves)
-
-    # End it
-    if is_stuck (move_area, game_state) and max (move_area) == 0:
-        print (f"MOVE {game_state['turn']}: I will go out on my own terms!")
-
-        if game_state["you"]["body"][1]["x"] < game_state["you"]["body"][0]["x"]:  # Neck is left of head, move left
-            move = "left"
-        elif game_state["you"]["body"][1]["x"] > game_state["you"]["body"][0]["x"]:  # Neck is right of head, move right
-            move = "right"
-        elif game_state["you"]["body"][1]["y"] < game_state["you"]["body"][0]["y"]:  # Neck is below head, move down
-            move = "down"
-        elif game_state["you"]["body"][1]["y"] > game_state["you"]["body"][0]["y"]:  # Neck is above head, move up
-            move = "up"
-        else:
-            move = "down"
-
-    print (f"MOVE {game_state['turn']}: {move}")
-    return {"move": move}
+# def move (game_state):
+#     """
+#     Called when the Battlesnake Engine needs to know your next move.
+#     The data parameter will contain information about the board.
+#     Your response must include your move of up, down, left, or right.
+#     """
+#     game_state
+#     move = ""
+#     up_area = floodFill (getNextPosition ("up", game_state), game_state, arrayify (game_state, not Beeg (game_state)))
+#     down_area = floodFill (getNextPosition ("down", game_state), game_state, arrayify (game_state, not Beeg (game_state)))
+#     right_area = floodFill (getNextPosition ("right", game_state), game_state, arrayify (game_state, not Beeg (game_state)))
+#     left_area = floodFill (getNextPosition ("left", game_state),  game_state, arrayify (game_state, not Beeg (game_state)))
+#     move_area = [up_area, down_area, right_area, left_area]
+#
+#     if len (game_state["board"]["food"]) > 0:
+#         print ("Current position: {}".format (game_state["you"]["head"]))
+#         print ("Closest Food: {}".format (findFood (game_state)))
+#         move = goto (move_area, findFood (game_state), game_state)
+#
+#     if is_stuck (move_area, game_state) and max (move_area) != 0:
+#         if up_area == max (move_area):
+#             move = "up"
+#         elif down_area == max (move_area):
+#             move = "down"
+#         elif left_area == max (move_area):
+#             move = "left"
+#         elif right_area == max (move_area):
+#             move = "right"
+#
+#     # Failsafe on
+#     if is_stuck (move_area, game_state) and max (move_area) == 0:
+#         print ("Failsafe triggered")
+#
+#     # Ignores projected heads
+#     if max (move_area) == 0:
+#         print ("Head prediction off")
+#         up_area = floodFill (getNextPosition ("up", game_state), game_state, arrayify (game_state, False))
+#         down_area = floodFill (getNextPosition ("down", game_state), game_state, arrayify (game_state, False))
+#         right_area = floodFill (getNextPosition ("right", game_state), game_state, arrayify (game_state, False))
+#         left_area = floodFill (getNextPosition ("left", game_state),  game_state, arrayify (game_state, False))
+#         move_area = [up_area, down_area, right_area, left_area]
+#
+#     if move == "":
+#         goodMoves = []
+#
+#         if up_area == max (move_area):
+#             goodMoves.append ("up")
+#
+#         if down_area == max (move_area):
+#             goodMoves.append ("down")
+#
+#         if left_area == max (move_area):
+#             goodMoves.append ("left")
+#
+#         if right_area == max (move_area):
+#             goodMoves.append ("right")
+#
+#         if (len (goodMoves) > 0):
+#             move = random.choice (goodMoves)
+#
+#     # End it
+#     if is_stuck (move_area, game_state) and max (move_area) == 0:
+#         print (f"MOVE {game_state['turn']}: I will go out on my own terms!")
+#
+#         if game_state["you"]["body"][1]["x"] < game_state["you"]["body"][0]["x"]:  # Neck is left of head, move left
+#             move = "left"
+#         elif game_state["you"]["body"][1]["x"] > game_state["you"]["body"][0]["x"]:  # Neck is right of head, move right
+#             move = "right"
+#         elif game_state["you"]["body"][1]["y"] < game_state["you"]["body"][0]["y"]:  # Neck is below head, move down
+#             move = "down"
+#         elif game_state["you"]["body"][1]["y"] > game_state["you"]["body"][0]["y"]:  # Neck is above head, move up
+#             move = "up"
+#         else:
+#             move = "down"
+#
+#     print (f"MOVE {game_state['turn']}: {move}")
+#     return {"move": move}
 
 
 def end (game_state):
@@ -327,4 +327,4 @@ def goto (move_area, pos, data):
 if __name__ == "__main__":
     from server import run_server
 
-    run_server ({"info": info, "start": start, "move": move, "end": end}, 8001)
+    run_server  ({"info": info, "start": start, "move": move, "end": end}, 8001)
